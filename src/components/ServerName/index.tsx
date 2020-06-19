528ce7409fa9ba768c1f071976e6214e645f4fd4
gustavo867 <tartatavo3@gmail.com>
2020-06-18 21:15:51 -0300

    :tada: discord clone

---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Title, ExpandIcon } from './styles'
+
+const ServerName: React.FC = () => {
+  return (
+        <Container>
+          <Title>Madeira Carai</Title>
+
+          <ExpandIcon/>
+        </Container>
+  );
+};
+
+export default ServerName;
